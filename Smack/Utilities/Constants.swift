ec00583e84cdd779ca065e60fb46b482e7523594
ellarobertson <ella.a.robertson@gmail.com>
2018-06-11 15:12:20 -0500

    created func for web request

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URLs
+let BASE_URL = "https://smack-chat-application.herokuapp.com/v1/"
+let REGISTER_URL = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
