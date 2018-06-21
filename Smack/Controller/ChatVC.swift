23e1c5c203b1ce319464c21bbfe9217c6fc45d79
ellarobertson <ella.a.robertson@gmail.com>
2018-06-20 19:07:55 -0500

    created messages struct/ability to retrive messages

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -50,16 +50,28 @@ class ChatVC: UIViewController {
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLbl.text = "#\(channelName)"
+        getMessages()
     }
     
     func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in
             if success {
-                //do stuff with channels
+                if MessageService.instance.channels.count > 0 {
+                    MessageService.instance.selectedChannel = MessageService.instance.channels[0]
+                    self.updateWithChannel()
+                } else {
+                    self.channelNameLbl.text = "No channels yet!"
+                }
             }
         }
     }
     
+    func getMessages() {
+        guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+        MessageService.instance.findAllMessagesForChannel(channelID: channelId) { (success) in
+            
+        }
+    }
 
 
 }
---
+++ Smack/Model/Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Ella Robertson on 6/20/18.
+//  Copyright © 2018 Ella Robertson. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+    
+}
